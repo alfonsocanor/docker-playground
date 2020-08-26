b76b25e8ca949183871f1842a21446759e575de3
Luis Alfonso Cano <33437646+alfonsocanor@users.noreply.github.com>
2020-08-26 20:16:46 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+'use strict';
+
 const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
 const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
 
-app.get('/', (req, res) => res.send('hello world');
-
-app.listen(3000, ()=> {
-    console.log('Node from docker');
-})
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
